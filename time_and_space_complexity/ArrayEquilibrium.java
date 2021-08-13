87f60d84b6291d8256cc83e4a74890e4e6415a78
Ashish Kulkarni <80530827+Ashishkulkarni00@users.noreply.github.com>
2021-08-13 19:19:45 +0530

    Add files via upload

---
+++ time_and_space_complexity/ArrayEquilibrium.java
@@ -0,0 +1,28 @@
+package time_and_space_complexity;
+
+public class ArrayEquilibrium {
+
+	public static int arrayEquilibrium(int[] arr) {
+		int rightSum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			rightSum += arr[i];
+		}
+		// full array total sum
+
+		int leftSum = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+
+			rightSum -= arr[i]; // totalsum-arr[i]
+			if (leftSum == rightSum) { // check leftSum==rightSum
+				return i;
+			}
+
+			leftSum += arr[i]; // next element leftSum
+
+		}
+		return -1;
+
+	}
+
+}
---
+++ time_and_space_complexity/DuplicatesInArray.java
@@ -0,0 +1,19 @@
+package time_and_space_complexity;
+
+public class DuplicatesInArray {
+	public static int duplicate(int[] arr) {
+
+		int n = arr.length - 2;
+		int sum = 0;
+		for (int i : arr) {
+			sum += i;
+		}
+		return sum - (n * (n + 1) / 2);
+
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 0, 7, 2, 5, 4, 7, 1, 3, 6 };
+		System.out.println(duplicate(arr));
+	}
+}
